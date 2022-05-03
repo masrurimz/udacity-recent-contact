a98d62b9b8463d5fcea6006f009552a24be778e8
Muhammad Zahid Masruri <masruri03@gmail.com>
2022-05-03 21:48:04 +0700

    Add ContactList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+interface Person {
+	name: string;
+}
+
+interface ContactListProps {
+	contacts: Person[];
+}
+
+export function ContactList({ contacts }: ContactListProps) {
+	return (
+		<ol>
+			{contacts.map((person) => (
+				<li>{person.name}</li>
+			))}
+		</ol>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ContactList
+				contacts={[{ name: "Michael" }, { name: "Ryan" }, { name: "Tyler" }]}
+			/>
+			<ContactList
+				contacts={[{ name: "Amanda" }, { name: "Richard" }, { name: "Geoff" }]}
+			/>
+		</div>
+	);
 }
 
 export default App;
