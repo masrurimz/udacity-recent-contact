ddbc81563f8caf651a4e897123f4c5ee5c6f4f21
Muhammad Zahid Masruri <masruri03@gmail.com>
2022-05-05 17:21:24 +0700

    Display contact avatar, name, email, and remove button

--- src/ListContacts.tsx
+++ src/ListContacts.tsx
@@ -1,3 +1,13 @@
+import {
+	HStack,
+	IconButton,
+	Image,
+	List,
+	ListItem,
+	VStack,
+} from "@chakra-ui/react";
+import { MdClose } from "react-icons/md";
+
 import { Contact } from "./App";
 
 interface ContactListProps {
@@ -6,10 +16,36 @@ interface ContactListProps {
 
 export function ListContacts({ contacts }: ContactListProps) {
 	return (
-		<ol>
+		<List spacing={3}>
 			{contacts.map((person) => (
-				<li key={person.id}>{person.name}</li>
+				<ListItem
+					key={person.id}
+					p={5}
+					m={{ base: 0, md: 5 }}
+					borderWidth={{ base: 0, md: 1 }}
+					rounded={{ base: 0, md: "md" }}>
+					<HStack spacing={5}>
+						<Image
+							boxSize={"60px"}
+							src={person.avatarURL}
+							alt={`${person.name} contact avatar`}
+							rounded={"full"}
+						/>
+						<VStack flex={1} alignItems={"flex-start"}>
+							<p>{person.name}</p>
+							<p>{person.email}</p>
+						</VStack>
+						<IconButton
+							rounded="full"
+							p={1}
+							colorScheme="gray"
+							aria-label={`Delete contact ${person.name}`}
+							icon={<MdClose />}
+							fontSize={32}
+						/>
+					</HStack>
+				</ListItem>
 			))}
-		</ol>
+		</List>
 	);
 }
