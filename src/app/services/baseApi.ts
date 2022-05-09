7a11c058b39012b1f21b25350cc1efb1cf9770a1
Muhammad Zahid Masruri <masruri03@gmail.com>
2022-05-09 12:11:37 +0700

    Config rtk query and redux

---
+++ src/app/services/baseApi.ts
@@ -0,0 +1,24 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseUrl =
+	process.env.REACT_APP_CONTACTS_API_URL || "http://localhost:5001";
+
+let token = localStorage.token;
+
+if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+	reducerPath: "api",
+	baseQuery: fetchBaseQuery({
+		baseUrl,
+		prepareHeaders: (headers) => {
+			headers.set("Accept", "application/json");
+			headers.set("Authorization", token);
+
+			return headers;
+		},
+	}),
+	endpoints: () => ({}),
+});
